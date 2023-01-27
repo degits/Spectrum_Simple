47461eaf0ccd0b1ed68e15d67451d9b73b0e1aae
DilshanGits <tdm.dev2@gmail.com>
2023-01-27 06:35:39 +0530

    source copy_1

---
+++ app/src/main/java/com/example/spectrumsimple/model/TranscodeState.kt
@@ -0,0 +1,14 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+//package com.facebook.spectrum.sample.model
+package com.example.spectrumsimple.model
+
+enum class TranscodeState {
+  NOT_STARTED,
+  RUNNING,
+  FINISHED_SUCCESS,
+  FINISHED_ERROR
+}
