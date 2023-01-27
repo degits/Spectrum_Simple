a1a9e9c2021319e7d3385af1a11f04847100a5a3
DilshanGits <tdm.dev2@gmail.com>
2023-01-27 06:44:45 +0530

    source copy_2

---
+++ app/src/main/java/com/example/spectrumsimple/DefaultImage.kt
@@ -0,0 +1,27 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+//
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+package com.example.spectrumsimple
+
+import android.content.Context
+import android.net.Uri
+import com.example.spectrumsimple.R
+import java.io.File
+import java.io.FileOutputStream
+
+private fun localFilePath(context: Context) = File(context.filesDir, "default_image.jpg")
+
+private fun ensureFileOnDisk(context: Context, localPath: File) {
+  if (!localPath.exists()) {
+    context.resources.openRawResource(R.raw.default_image).copyTo(FileOutputStream(localPath))
+  }
+}
+
+@JvmName("getDefaultImageUri")
+fun defaultImageUri(context: Context): Uri {
+  val localPath = localFilePath(context)
+  ensureFileOnDisk(context, localPath)
+  return Uri.fromFile(localPath)
+}
